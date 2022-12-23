f4e45a8183f7773eb7a8c9ee4d461211de595130
CastellucciLuca <castellucciluca98@gmail.com>
2022-12-23 14:45:07 +0100

    add bonus

---
+++ vite-hello/src/components/AppHero.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <img src="https://www.hotelparigi2.it/wp-content/uploads/2015/11/Bergamo-e-la-magia-del-Natale-mercatini-di-natale-Bergamo.jpg" alt="">
+</template>
+
+<style scoped>
+
+</style>
